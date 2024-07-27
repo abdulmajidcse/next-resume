7cb21360a84aa26ca5b45e1b45a109b016acbd0e
Abdul Majid <abdulmajidcse@gmail.com>
2024-07-28 00:11:32 +0600

    added not found page.

---
+++ app/not-found.tsx
@@ -0,0 +1,13 @@
+export default function NotFound() {
+  return (
+    <div className="min-h-[calc(100vh-200px)] flex items-center justify-center font-lato">
+      <div className="pr-4 text-lg text-gray-500 border-r border-gray-400 tracking-wider">
+        404{" "}
+      </div>
+
+      <div className="ml-4 text-lg text-gray-500 uppercase tracking-wider">
+        Not Found{" "}
+      </div>
+    </div>
+  );
+}
