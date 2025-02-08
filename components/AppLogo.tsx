9ff700daf00bb9ab9e0af2ec3b352f022add4f0d
abdulmajidcse <abdulmajidcse@gmail.com>
2025-02-08 23:12:35 +0600

    Update logo.

--- components/AppLogo.tsx
+++ components/AppLogo.tsx
@@ -1,10 +1,19 @@
+import Image from "next/image";
+
 const AppLogo = () => {
   return (
-    <div className="bg-black p-1 rounded-lg">
-      <span className="self-center text-3xl font-extrabold whitespace-nowrap text-white tracking-tighter">
-        AM
-      </span>
-    </div>
+    <Image
+      src="/logo.png"
+      width={211}
+      height={225}
+      className="w-10 h-auto"
+      alt="AM"
+    />
+    // <div className="bg-black p-1 rounded-lg">
+    //   <span className="self-center text-3xl font-extrabold whitespace-nowrap text-white tracking-tighter">
+    //     AM
+    //   </span>
+    // </div>
   );
 };
 
