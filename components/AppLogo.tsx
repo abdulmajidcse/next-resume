dcbd178ed4878ec282becf995163a4c8cb25f8d3
abdulmajidcse <abdulmajidcse@gmail.com>
2025-02-08 21:51:57 +0600

    Update logo.

--- components/AppLogo.tsx
+++ components/AppLogo.tsx
@@ -1,10 +1,10 @@
 const AppLogo = (props: { className?: string }) => {
   return (
-    <span
-      className={`self-center text-3xl font-extrabold whitespace-nowrap theme-text-shadow tracking-tighter ${props.className}`}
-    >
-      AM
-    </span>
+    <div className={`bg-black p-1 rounded-lg ${props.className}`}>
+      <span className="self-center text-3xl font-extrabold whitespace-nowrap theme-text-shadow tracking-tighter">
+        AM
+      </span>
+    </div>
   );
 };
 
