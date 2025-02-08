53465b25830d03a5a6d9a5cba3125b1305da878e
abdulmajidcse <abdulmajidcse@gmail.com>
2025-02-08 18:04:32 +0600

    Add projects section.

--- app/page.tsx
+++ app/page.tsx
@@ -95,10 +95,106 @@ export default function Home() {
         </div>
       </section>
 
+      {/* Projects */}
+      <section className="section">
+        <h2 className="theme-text-color text-xl font-bold">Projects</h2>
+        <div className="mt-6">
+          <div className="text-sm flex flex-col gap-4 my-4 text-slate-900">
+            <p>
+              <a
+                href="https://lib.bim.gov.bd"
+                className="underline hover:theme-text-color decoration-green-600"
+              >
+                BIM Library
+              </a>
+              : It&apos;s a Library Management System. It is also a smart LMS
+              System where you may simplify your workflow, easy cataloging to
+              seamless eBook migration and all! It is a automated, RFID &
+              barcode system, instant & various types of reports, cataloging,
+              patron management, migration of eBook from Koha, circulation
+              management, integration with e-content library and more.
+            </p>
+
+            <p>
+              <a
+                href="http://203.82.196.38"
+                className="underline hover:theme-text-color decoration-green-600"
+              >
+                Naasa Group ERP
+              </a>
+              : A modern and reliable ERP for{" "}
+              <a
+                href="https://nassa.com.bd"
+                className="underline hover:theme-text-color decoration-green-600"
+              >
+                Naasa Group
+              </a>{" "}
+              where they work on Garment Manufacturing, Banking, Real Estate,
+              Stock Brokering, Education and Travel and most importantly
+              Corporate Social Responsibility.
+            </p>
+
+            <p>
+              <a
+                href="https://aosed.org"
+                className="underline hover:theme-text-color decoration-green-600"
+              >
+                Aosed
+              </a>
+              : A modern and smart website & HRM management system for Aosed. It
+              has a content management system and HRM management.
+            </p>
+
+            <p>
+              <a
+                href="https://cricket97.com"
+                className="underline hover:theme-text-color decoration-green-600"
+              >
+                Cricket97
+              </a>
+              : An online newspaper website based on Cricket. It has a modern,
+              reliable and & effecient CMS for newspaper.
+            </p>
+
+            <p>
+              <a
+                href="https://fulyco.us"
+                className="underline hover:theme-text-color decoration-green-600"
+              >
+                Fulyco
+              </a>
+              : It is a on demand gas delivery service and based on USA.
+            </p>
+
+            <p>
+              <a
+                href="https://examcare24.com"
+                className="underline hover:theme-text-color decoration-green-600"
+              >
+                Examcare24
+              </a>
+              : An online based exam management system and fully automated,
+              exam, results, reports, payment system and more.
+            </p>
+
+            <p>
+              <a
+                href="https://studio23.com.bd"
+                className="underline hover:theme-text-color decoration-green-600"
+              >
+                Studio23
+              </a>
+              : A hotel website where it represents about hotel, services,
+              simple booking feature, and more.
+            </p>
+          </div>
+        </div>
+      </section>
+
       {/* my popular medium stories */}
       <section className="section">
         <h2 className="theme-text-color text-xl font-bold">
-          My Popular Medium Stories
+          Popular Medium Stories
         </h2>
         <div className="mt-6">
           <div className="text-sm flex flex-col gap-4 my-4 text-slate-900">
@@ -173,7 +269,7 @@ export default function Home() {
       {/* my github learning resources */}
       <section className="section">
         <h2 className="theme-text-color text-xl font-bold">
-          My Github Learning Resources
+          Github Learning Resources
         </h2>
         <div className="mt-6">
           <div className="text-sm flex flex-col gap-4 my-4 text-slate-900">
@@ -251,9 +347,7 @@ export default function Home() {
       </section>
       {/* my learning screencasts */}
       <section className="section">
-        <h2 className="theme-text-color text-xl font-bold">
-          My Youtube Learning Screencasts
-        </h2>
+        <h2 className="theme-text-color text-xl font-bold">Screencasts</h2>
         <div className="mt-6">
           <div className="text-sm flex flex-col gap-10 my-4 text-slate-900">
             <div>
