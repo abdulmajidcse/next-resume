df24c336d9142370f4d767dccff5e489c899a676
Md Abdul Majid <abdulmajidcse@gmail.com>
2025-03-21 14:48:11 +0600

    Remove unnecessary configuration.

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-// only for docker development
-// const nextConfig = {
-//   webpack: (config, _) => ({
-//     ...config,
-//     watchOptions: {
-//       ...config.watchOptions,
-//       poll: 800,
-//       aggregateTimeout: 300,
-//     },
-//   }),
-// };
-
 const nextConfig = {};
 
 export default nextConfig;
